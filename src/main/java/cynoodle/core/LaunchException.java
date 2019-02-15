9a4902490a76a069ccf3a36278dfa0d03babf9a6
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-15 13:03:05 +0100

    removed Launcher (temp, for repo only)

---
+++ src/main/java/cynoodle/core/LaunchException.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) enveeed 2019 - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package cynoodle.core;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Thrown by {@link CyNoodle#launch(StartParameters)} to indicate that launching failed.
+ */
+public final class LaunchException extends Exception {
+
+    LaunchException(@Nonnull Throwable cause) {
+        super(cause);
+    }
+}
