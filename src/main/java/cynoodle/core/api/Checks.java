fa5175f6f16428e3bac4969efeb28158efeeb203
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-17 16:38:05 +0100

    added empty and blank checks for Strings

--- src/main/java/cynoodle/core/api/Checks.java
+++ src/main/java/cynoodle/core/api/Checks.java
@@ -9,7 +9,7 @@ package cynoodle.core.api;
 import javax.annotation.Nonnull;
 
 /**
- * Validity checks for single values.
+ * validity checks for single values.
  */
 public final class Checks {
     private Checks() {}
@@ -24,8 +24,33 @@ public final class Checks {
 
     @Nonnull
     public static <T> T notNull(T value) throws NullPointerException {
-        if(value == null) throw new NullPointerException("Value cannot be null!");
+        return notNull(value, "value");
+    }
+
+    // === STRINGS ===
+
+    @Nonnull
+    public static String notEmpty(String value, @Nonnull String name) throws NullPointerException, IllegalArgumentException {
+        Checks.notNull(value, name);
+        if(value.isEmpty()) throw new IllegalArgumentException(name + " cannot be empty!");
         return value;
+    }
+
+    @Nonnull
+    public static String notEmpty(String value) throws NullPointerException, IllegalArgumentException {
+        return notEmpty(value, "value");
+    }
+
+    @Nonnull
+    public static String notBlank(String value, @Nonnull String name) throws NullPointerException, IllegalArgumentException {
+        Checks.notNull(value, name);
+        if(value.isBlank()) throw new IllegalArgumentException(name + " cannot be blank!");
+        return value;
+    }
+
+    @Nonnull
+    public static String notBlank(String value) throws NullPointerException, IllegalArgumentException {
+        return notBlank(value, "value");
     }
 
 }
