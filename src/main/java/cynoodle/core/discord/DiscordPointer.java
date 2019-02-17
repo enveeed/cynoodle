cce27152d66b5cc1a580b2af4d16a853b0834448
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-17 20:32:18 +0100

    docs and improvements for DiscordPointer

--- src/main/java/cynoodle/core/discord/DiscordPointer.java
+++ src/main/java/cynoodle/core/discord/DiscordPointer.java
@@ -11,34 +11,50 @@ import net.dv8tion.jda.core.entities.ISnowflake;
 import javax.annotation.Nonnull;
 
 /**
- * Immutable pointer to a uniquely snowflake-identified entity in Discord.
+ * Immutable pointer to any snowflake-identified entity within Discord.
  */
 public final class DiscordPointer {
 
     private final long id;
 
-    //
+    // ===
 
     private DiscordPointer(long id) {
         this.id = id;
     }
 
-    //
+    // ===
 
+    /**
+     * Get the snowflake ID of the entity this pointer points to.
+     * @return the pointer target ID
+     */
     public long getID() {
         return this.id;
     }
 
-    //
+    // ===
 
+    /**
+     * Create a new pointer to the given snowflake ID.
+     * @param snowflake the snowflake ID
+     * @return a new pointer pointing to the given ID.
+     * @throws IllegalArgumentException if the snowflake ID was malformed
+     */
     @Nonnull
-    public static DiscordPointer to(long snowflake) {
-        if(snowflake <= 0L) throw new IllegalArgumentException("Illegal snowflake: "+snowflake);
+    public static DiscordPointer to(long snowflake) throws IllegalArgumentException {
+        if(snowflake <= 0L) throw new IllegalArgumentException("Illegal snowflake ID: "+snowflake);
         return new DiscordPointer(snowflake);
     }
 
+    /**
+     * Create a new pointer to the snowflake ID of the given JDA {@link ISnowflake}.
+     * @param entity the JDA {@link ISnowflake} with the ID.
+     * @return a new pointer pointing to the ID of the given JDA entity
+     * @throws IllegalArgumentException if the snowflake ID of the given entity was malformed
+     */
     @Nonnull
     public static DiscordPointer to(@Nonnull ISnowflake entity) {
-        return new DiscordPointer(entity.getIdLong());
+        return to(entity.getIdLong());
     }
 }
