bb5e1093a93bbdcb8f9e3f38e2db2b2c14af75d3
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-18 19:25:32 +0100

    implemented ParserException

--- src/main/java/cynoodle/core/api/text/ParserException.java
+++ src/main/java/cynoodle/core/api/text/ParserException.java
@@ -6,5 +6,20 @@
 
 package cynoodle.core.api.text;
 
-public class ParserException extends Exception {
+public final class ParserException extends Exception {
+
+    public ParserException() {
+    }
+
+    public ParserException(String message) {
+        super(message);
+    }
+
+    public ParserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParserException(Throwable cause) {
+        super(cause);
+    }
 }
