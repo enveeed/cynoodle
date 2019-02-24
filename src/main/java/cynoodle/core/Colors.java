56161fc81f53d241483b35d6eed7c80dbacc4f77
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-24 20:14:20 +0100

    added Colors utility

---
+++ src/main/java/cynoodle/core/Colors.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) enveeed 2019 - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package cynoodle.core;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Static utility for colors.
+ */
+public final class Colors {
+    private Colors() {}
+
+    // ===
+
+    @Nonnull
+    public static Color averageByImage(@Nonnull BufferedImage image) {
+
+        long sumr = 0, sumg = 0, sumb = 0;
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                Color pixel = new Color(image.getRGB(x, y));
+                sumr += pixel.getRed();
+                sumg += pixel.getGreen();
+                sumb += pixel.getBlue();
+            }
+        }
+
+        int num = image.getWidth() * image.getHeight();
+
+        return new Color(
+                (int) sumr / num,
+                (int) sumg / num,
+                (int) sumb / num);
+
+    }
+}
