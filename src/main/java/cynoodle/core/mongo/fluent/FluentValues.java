2664f1e88f35c556fb8da3bfa497fe47b980383c
enveeed <25058915+enveeed@users.noreply.github.com>
2019-02-28 19:26:42 +0100

    added FluentValues converters for temperature and mass units

--- src/main/java/cynoodle/core/mongo/fluent/FluentValues.java
+++ src/main/java/cynoodle/core/mongo/fluent/FluentValues.java
@@ -6,48 +6,73 @@
 
 package cynoodle.core.mongo.fluent;
 
+import cynoodle.core.api.measurements.MassUnit;
+import cynoodle.core.api.measurements.TemperatureUnit;
 import org.bson.BsonInt64;
-import org.bson.BsonNull;
+import org.bson.BsonString;
 import org.bson.BsonValue;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Currency;
 import java.util.function.Function;
 
 public final class FluentValues {
     private FluentValues() {}
 
-    // ===
+    // time
 
     public static Function<Instant, BsonInt64> fromInstant() {
         return x -> new BsonInt64(x.toEpochMilli());
-    }
-
-    public static Function<Instant, BsonValue> fromInstantNullable() {
-        return x -> x == null ? BsonNull.VALUE : new BsonInt64(x.toEpochMilli());
     }
 
     public static Function<BsonValue, Instant> toInstant() {
         return x -> Instant.ofEpochMilli(x.asInt64().getValue());
     }
 
-    public static Function<BsonValue, Instant> toInstantNullable() {
-        return x -> x.isNull() ? null : Instant.ofEpochMilli(x.asInt64().getValue());
-    }
-
     public static Function<Duration, BsonInt64> fromDuration() {
         return x -> new BsonInt64(x.toMillis());
-    }
-
-    public static Function<Duration, BsonValue> fromDurationNullable() {
-        return x -> x == null ? BsonNull.VALUE : new BsonInt64(x.toMillis());
     }
 
     public static Function<BsonValue, Duration> toDuration() {
         return x -> Duration.ofMillis(x.asInt64().getValue());
     }
 
-    public static Function<BsonValue, Duration> toDurationNullable() {
-        return x -> x.isNull() ? null : Duration.ofMillis(x.asInt64().getValue());
+    public static Function<ZoneId, BsonString> fromZoneId() {
+        return x -> new BsonString(x.getId());
     }
+
+    public static Function<BsonValue, ZoneId> toZoneId() {
+        return x -> ZoneId.of(x.asString().getValue());
+    }
+
+    // other
+
+    public static Function<Currency, BsonString> fromCurrency() {
+        return x -> new BsonString(x.getCurrencyCode());
+    }
+
+    public static Function<BsonValue, Currency> toCurrency() {
+        return x -> Currency.getInstance(x.asString().getValue());
+    }
+
+    // === UNITS OF MEASUREMENT ===
+
+    public static Function<TemperatureUnit, BsonString> fromTemperatureUnit() {
+        return x -> new BsonString(x.getIdentifier());
+    }
+
+    public static Function<BsonValue, TemperatureUnit> toTemperatureUnit() {
+        return x -> TemperatureUnit.of(x.asString().getValue()).orElseThrow();
+    }
+
+    public static Function<MassUnit, BsonString> fromMassUnit() {
+        return x -> new BsonString(x.getIdentifier());
+    }
+
+    public static Function<BsonValue, MassUnit> toMassUnit() {
+        return x -> MassUnit.of(x.asString().getValue()).orElseThrow();
+    }
+
 }
