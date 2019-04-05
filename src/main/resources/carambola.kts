f3773f4d8ea38ab0f1d2ca9acb0bb4a710d4cf3c
enveeed <25058915+enveeed@users.noreply.github.com>
2019-04-05 10:39:19 +0200

    added useJUL() to carambola.kts

--- src/main/resources/carambola.kts
+++ src/main/resources/carambola.kts
@@ -5,18 +5,28 @@
  */
 
 import enveeed.carambola.dsl.*
-import enveeed.carambola.flogger.dsl.*
+
+import enveeed.carambola.flogger.dsl.useFlogger
+import enveeed.carambola.slf4j.dsl.useSLF4J
+import enveeed.carambola.jul.dsl.useJUL
+
 import enveeed.carambola.handlers.StandardHandler
+
+import java.util.logging.Level
 
 // carambola configuration script
 carambola {
 
     adapters {
         useFlogger()
+        useSLF4J()
+        useJUL()
     }
 
     handlers {
         handler(StandardHandler())
     }
+
+    level(Level.FINE)
 
 }
