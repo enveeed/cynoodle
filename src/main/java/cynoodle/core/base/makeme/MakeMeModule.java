fc8718a2eb031466de676b6f80f53bc061fa393d
enveeed <25058915+enveeed@users.noreply.github.com>
2019-04-05 10:57:22 +0200

    removed test setup code in base:makeme

--- src/main/java/cynoodle/core/base/makeme/MakeMeModule.java
+++ src/main/java/cynoodle/core/base/makeme/MakeMeModule.java
@@ -6,9 +6,8 @@
 
 package cynoodle.core.base.makeme;
 
-import cynoodle.core.base.commands.CommandsModule;
 import cynoodle.core.base.commands.CommandRegistry;
-import cynoodle.core.discord.DiscordPointer;
+import cynoodle.core.base.commands.CommandsModule;
 import cynoodle.core.discord.GEntityManager;
 import cynoodle.core.discord.MEntityManager;
 import cynoodle.core.entities.EntityType;
@@ -58,38 +57,6 @@ public final class MakeMeModule extends Module {
 
         registry.register(MakeMeCommand.class);
         registry.register(UnMakeMeCommand.class);
-
-        // === TEST ONLY ===
-
-        if(noodle().getLaunchSettings().isSetupTestEnabled()) {
-
-            DiscordPointer guild = DiscordPointer.to(394436622994636801L);
-
-            MakeMeController.OnGuild controller = controller().onGuild(guild);
-
-            controller.all().forEach(MakeMe::delete);
-            controller.allGroups().forEach(MakeMeGroup::delete);
-
-            MakeMe mm_a = controller.create("mm_a", "Test Make-Me A", DiscordPointer.to(555107066138591253L));
-            MakeMe mm_b = controller.create("mm_b", "Test Make-Me B", DiscordPointer.to(556866896662757376L));
-            MakeMe mm_c = controller.create("mm_c", "Test Make-Me C", DiscordPointer.to(556866931995443211L));
-            MakeMe mm_d = controller.create("mm_d", "Test Make-Me D", DiscordPointer.to(556866974697652235L));
-            MakeMe mm_e = controller.create("mm_e", "Test Make-Me E", DiscordPointer.to(556867020281610271L));
-            MakeMe mm_f = controller.create("mm_f", "Test Make-Me F", DiscordPointer.to(556867096181473322L));
-
-            MakeMeGroup group_first = controller.createGroup("mm_first", "Test Group I");
-            MakeMeGroup group_second = controller.createGroup("mm_second", "Test Group II");
-
-            mm_a.setGroup(group_first);
-            mm_b.setGroup(group_first);
-            mm_c.setGroup(group_first);
-            mm_d.setGroup(group_second);
-            mm_e.setGroup(group_second);
-            mm_f.setGroup(null);
-
-            getMakeMeManager().persistAll(mm_a, mm_b, mm_c, mm_d, mm_e, mm_f);
-            getGroupManager().persistAll(group_first, group_second);
-        }
     }
 
     @Override
